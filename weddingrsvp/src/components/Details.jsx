8b66dc8160fe2e962b683d5fddf6bf5c027aa483
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-09-23 17:42:41 -0500

    details and form

---
+++ weddingrsvp/src/components/Details.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Details = () => {
+    return (
+        <div>
+            <h2>The Details</h2>
+            <p>Location: The Gardens Wedding Center</p>
+            <p>Date:August 2nd, 2025</p>
+            <p>Time: 3:00pm</p>
+        </div>
+    );
+};
+
+export default Details;
---
+++ weddingrsvp/src/components/RSVPForm.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const RSVPForm = () => {
+    const initialValues = {
+        guests: '',
+        dinnerChoice: ''
+    };
+
+    const validationSchema = Yup.object({
+        guests: Yup.number().required('Required'), //.min(1, 'Must be at least 1').max(10, 'Must be 10 or fewer') commented out for testing
+        dinnerChoice: Yup.string().required('Required')
+    });
+
+    const onSubmit = values => {
+        console.log(values);
+    };
+
+    return (
+        <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}>
+                <Form>
+                    <div>
+                        <label htmlFor="guests">Number of Guests</label>
+                        <Field type="number" id="guests" name="guests"/>
+                        <ErrorMessage name="guests" component="div"/>
+                    </div>
+                    <div>
+                        <label htmlFor="dinnerChoice">Dinner Choice</label>
+                        <Field as="select" id="dinnerChoice" name="dinnerChoice">
+                            <option value="">Select</option>
+                            <option value="beef">Beef</option>
+                            <option value="chicken">Chicken</option>
+                            <option value="vegetarian">Vegetarian</option>
+                        </Field>
+                        <ErrorMessage name="dinnerChoice" component="div"/>
+                    </div>
+                    <button type="submit">Submit</button>
+                </Form>
+        </Formik>
+    );
+};
+
+export default RSVPForm;
